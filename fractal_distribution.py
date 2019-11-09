66e359f163055aea42c4c394d14ea604b07d4ab1
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-09 03:45:44 -0500

    Implement trade data generator

--- fractal_distribution.py
+++ fractal_distribution.py
@@ -1,6 +1,21 @@
 from numpy.random import permutation
 from numpy import concatenate
 from math import floor
+from random import randrange
+from tqdm import tqdm
+import os
+
+COLUMN_NAMES = ['stocksymbol', 'time', 'quantity', 'price']
+FILE_PATH = 'trade.csv'
+
+# NUM_RECORDS = 10000000
+# NUM_STOCKSYMBOLS = 100000
+# X = 70002
+
+# test
+NUM_RECORDS = 10000
+NUM_STOCKSYMBOLS = 1000
+X = 703
 
 
 def gen(frac, n):
@@ -13,6 +28,35 @@ def gen(frac, n):
 
 
 if __name__ == '__main__':
-    out = gen(0.3, 70002)
-    print(out)
-    print('size = {}'.format(out.size))  # size should be 100000
+    stocksymbol_sample = gen(0.3, X)
+
+    print('NUM_RECORDS = {}'.format(NUM_RECORDS))
+    print('NUM_STOCKSYMBOLS = {}'.format(stocksymbol_sample.size))
+
+    # output
+    if os.path.exists(FILE_PATH):
+        os.remove(FILE_PATH)
+    f = open(FILE_PATH, 'w')
+    # column names
+    f.write(','.join(COLUMN_NAMES) + '\n')
+    # data
+    prev_price_list = [-1 for i in range(NUM_STOCKSYMBOLS)]
+    for i in tqdm(range(NUM_RECORDS)):
+        stocksymbol = stocksymbol_sample[randrange(0, NUM_STOCKSYMBOLS)]
+        time = i
+        quantity = randrange(100, 10001)
+        price = randrange(50, 501)
+        if prev_price_list[stocksymbol - 1] != -1:
+            prev_price = prev_price_list[stocksymbol - 1]
+            while not (prev_price - 5 <= price <= prev_price - 1 or
+                       prev_price + 1 <= price <= prev_price + 5):
+                price = randrange(50, 501)
+        prev_price_list[stocksymbol - 1] = price
+
+        f.write(
+            str(stocksymbol) + ',' +
+            str(time) + ',' +
+            str(quantity) + ',' +
+            str(price) + '\n'
+        )
+    f.close()
