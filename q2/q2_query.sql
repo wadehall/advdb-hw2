cad882e3f128e79730c574b7b41a93d01e28f1d9
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-21 16:03:52 -0500

    Add q2 MySQL part

--- q2/q2_query.sql
+++ q2/q2_query.sql
@@ -1,3 +1,19 @@
+-- DROP TABLE IF EXISTS trade_fractal;
+-- CREATE TABLE trade_fractal (stocksymbol INT, time INT, quantity INT, price INT);
+--
+-- LOAD DATA INFILE "trade_fractal.csv"
+-- INTO TABLE trade_fractal
+-- FIELDS TERMINATED BY ","
+-- IGNORE 1 LINES;
+--
+-- DROP TABLE IF EXISTS trade_uniform;
+-- CREATE TABLE trade_uniform (stocksymbol INT, time INT, quantity INT, price INT);
+--
+-- LOAD DATA INFILE "trade_uniform.csv"
+-- INTO TABLE trade_uniform
+-- FIELDS TERMINATED BY ","
+-- IGNORE 1 LINES;
+
 /* Use Distinct */
 SELECT DISTINCT stocksymbol, time, quantity, price
 FROM trade
